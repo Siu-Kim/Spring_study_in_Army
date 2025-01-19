ac2ff25249a43853613b08e35b26b8a281b64e44
Siu-Kim <siwoo030407@gmail.com>
2025-01-19 14:47:05 +0000

    add main.class that has main method of WMS server side code, but not completed

---
+++ src/main/java/main/main.java
@@ -0,0 +1,20 @@
+import Cargo.*;
+import Warehouse.*;
+
+public class Main{
+    public static void main(String args[]){
+        
+        /*
+        CLI 입출력 + switch-case 문으로 main logic 호출
+        
+        
+        */
+        
+        
+        
+        
+        
+        
+    }
+
+}
